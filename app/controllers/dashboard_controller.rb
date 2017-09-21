8fbd20f4836c081d2d5d9666f7b2bbcd7182851e
aldompe95 <aldompe95@gmail.com>
2017-09-20 20:37:55 -0500

    Testing koala

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -4,7 +4,7 @@ class DashboardController < ApplicationController
   def home
     profile_exist?
     @requisitions = current_user.requisition
-    @movies = Facebook.get_object(current_user.token, '/me?fields=id,first_name,last_name')
+    @movies = Facebook.get_object(current_user.token, '/me?fields=id,first_name,last_name,bitrhday')
   end
 
   private
