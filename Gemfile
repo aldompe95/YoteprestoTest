91f1f7454b5713d979ae61861f989388b9424222
aldompe95 <aldompe95@gmail.com>
2017-09-20 20:30:44 -0500

    Testing koala

--- Gemfile
+++ Gemfile
@@ -5,6 +5,8 @@ git_source(:github) do |repo_name|
   "https://github.com/#{repo_name}.git"
 end
 
+# Use koala to request fb data
+gem 'koala'
 # Use dotenv for env
 gem 'dotenv-rails'
 # Use bootstrap
--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -4,6 +4,7 @@ class DashboardController < ApplicationController
   def home
     profile_exist?
     @requisitions = current_user.requisition
+    @movies = Facebook.get_object(current_user.token, '/me/movies?fields=name,picture,studio')
   end
 
   private
