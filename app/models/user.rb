eb651835fbacba90a5164dd55066f39be265c4b9
aldompe95 <aldompe95@gmail.com>
2017-09-20 20:06:30 -0500

    Fix relation of user and profile

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  #has_one :profile
+  has_one :profile
   has_many :requisition
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
